968be5f3e97a8455adb02a5e27c6a7c1748d4be5
Akilis16 <gabrielsebastiao16072005@gmail.com>
2024-12-28 14:24:05 -0300

    Service layer, component registration

---
+++ src/main/java/com/akilis16/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.akilis16.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.akilis16.course.entities.User;
+import com.akilis16.course.repositorys.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
